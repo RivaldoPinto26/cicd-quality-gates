aaece18fadc1a60633f358d115bb81ee860e38cc
Rivaldo Pinto <rivaldo.pinto1998@gmail.com>
2026-04-27 14:51:27 +0000

    add function and test substract

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,6 @@
 #simple calculator 
 def add(a, b):
     return a + b
+
+def subtract(a,b):
+    return a - b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,9 @@
 # tests will be added in PR #2 when functions are implemented
+
 from src.calculator import add, subtract
+
 def test_add():
     assert add(2, 3) == 5
 
-def test_substract():
-    assert subtract(4,2) == 2
+def test_subtract():
+    assert subtract(7, 3) == 4
